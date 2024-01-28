8677d5a131b37ab49d1ba234c0ba6a6b6e708853
Rodrigo Aleman <rodrigoal9715@gmail.com>
2024-01-28 17:11:58 -0600

    Add graphql-subscriptions package and create a PubSub instance for handling message subscriptions. Publish MESSAGE_ADDED event when a new message is added and update addMessage mutation to publish the event. Add Subscription type for subscribing to the messageAdded event.
    On the server side, import the PubSub class from graphql-subscriptions package and create a PubSub instance named pubSub in the resolvers.js file. Use this instance to publish the MESSAGE_ADDED event when a new message is added in the addMessage mutation. Subscription type is defined to allow clients to subscribe to the messageAdded event and receive updates when a new message is added.
    
    This commit adds support for message subscriptions using the graphql-subscriptions package.
    
    Please note that this analysis is based solely on the provided git diff and does not consider any context outside of it.

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,24 +1,34 @@
-import { GraphQLError } from 'graphql';
-import { createMessage, getMessages } from './db/messages.js';
+import {GraphQLError} from 'graphql';
+import {createMessage, getMessages} from './db/messages.js';
+import {PubSub} from "graphql-subscriptions";
 
+const pubSub = new PubSub();
 export const resolvers = {
-  Query: {
-    messages: (_root, _args, { user }) => {
-      if (!user) throw unauthorizedError();
-      return getMessages();
+    Query: {
+        messages: (_root, _args, {user}) => {
+            if (!user) throw unauthorizedError();
+            return getMessages();
+        },
     },
-  },
 
-  Mutation: {
-    addMessage: (_root, { text }, { user }) => {
-      if (!user) throw unauthorizedError();
-      return createMessage(user, text);
+    Mutation: {
+        addMessage: async (_root, {text}, {user}) => {
+            if (!user) throw unauthorizedError();
+            const message = await createMessage(user, text);
+            await pubSub.publish('MESSAGE_ADDED', {messageAdded: message})
+            return createMessage(user, text);
+        },
     },
-  },
+
+    Subscription: {
+        messageAdded: {
+            subscribe: () => pubSub.asyncIterator('MESSAGE_ADDED'),
+        }
+    }
 };
 
 function unauthorizedError() {
-  return new GraphQLError('Not authenticated', {
-    extensions: { code: 'UNAUTHORIZED' },
-  });
+    return new GraphQLError('Not authenticated', {
+        extensions: {code: 'UNAUTHORIZED'},
+    });
 }
